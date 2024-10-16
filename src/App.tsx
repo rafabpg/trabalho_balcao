cde37b8ebc406ca2c5ea477a04a7f3e64b83ed46
rafabpg <rafa23giordano@gmail.com>
2024-10-16 16:14:20 -0300

    feat: adding template with header and new image

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
+import AppRoutes from "./routes/routes"
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <AppRoutes />
   )
 }
 
