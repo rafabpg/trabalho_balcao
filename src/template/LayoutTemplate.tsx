ec8eb648e43a75fc4834d6fe79fcc01c5b5f8411
Lucas Levy <lucaslevy@id.uff.br>
2024-11-08 11:43:23 -0300

    feature add footer

--- src/template/LayoutTemplate.tsx
+++ src/template/LayoutTemplate.tsx
@@ -1,13 +1,17 @@
-import Header from '@/components/Organisms/Header'
-import { Outlet } from 'react-router'
+import Header from '@/components/Organisms/Header';
+import Footer from '@/components/Organisms/Footer';
+import { Outlet } from 'react-router';
 
 const LayoutTemplate = () => {
   return (
-    <main>
-        <Header/>
-        <Outlet/>
+    <main className="flex flex-col min-h-screen">
+      <Header />
+      <div className="flex-grow">
+        <Outlet />
+      </div>
+      <Footer />
     </main>
-  )
-}
+  );
+};
 
-export default LayoutTemplate
+export default LayoutTemplate;
