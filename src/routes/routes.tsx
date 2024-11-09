0e81e681445b718383029fdaa5300f61d42d168a
Lucas Levy <lucaslevy@id.uff.br>
2024-11-09 13:06:17 -0300

    fix/code

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,14 +1,14 @@
 import CreateAnnouncementPage from "@/template/CreateAnnouncementPage"
 import LayoutTemplate from "@/template/LayoutTemplate"
-import Home from '@/assets/pages/Home';
-import MyAds from '@/assets/pages/MyAds';
+import Home from '@/pages/Home';
+import MyAds from '@/pages/MyAds';
 import LoginPage from "@/pages/LoginPage"
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
         <Route path="/" element={<LayoutTemplate />}>
           <Route index element={<Home />} />
           <Route path="/meus-anuncios" element={<MyAds />} />
