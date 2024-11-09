72d973034101f5637ae9cf9e1d80b0993b5fd296
FellipePombo <fellipecarvalhop@gmail.com>
2024-11-09 19:26:22 -0300

    fix: fixing tailwind and connecting homepage to singleadpage

---
+++ src/components/Molecules/Tag.tsx
@@ -0,0 +1,24 @@
+import Loc from "@/assets/icons/local.png"
+import TagIcon from "../Atoms/TagIcon"
+
+interface TagProps{
+    text: string
+    categoria: boolean
+}
+
+const Tag = ({text,categoria}:TagProps) =>{
+    return(
+
+        <div className= "flex items-center text-center w-auto h-9 rounded-full mt-2  bg-lighter-primary">
+        <TagIcon categoria = {categoria}/>
+        <span className="text-lg text-secondary text-center text-nowrap">{text}</span></div>
+        
+
+    )
+
+
+
+
+} 
+
+export default Tag
