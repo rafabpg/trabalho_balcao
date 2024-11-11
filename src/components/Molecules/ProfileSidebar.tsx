8f65926bd835e361f5314dbe9b498ab79b53ce4c
rafabpg <rafa23giordano@gmail.com>
2024-11-11 20:20:33 -0300

    fix :fixing routes

--- src/components/Molecules/ProfileSidebar.tsx
+++ src/components/Molecules/ProfileSidebar.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import NavLinkItem from '../Atoms/NavLinkItem'
 import LinkItem from '../Atoms/LinkItem'
 import { FiLogOut } from 'react-icons/fi'
@@ -7,7 +6,7 @@ const ProfileSidebar = () => {
   return (
     <nav className='px-6 lg:px-14 bg-secondary pt-8 min-h-screen'>
         <ul className='flex flex-col gap-6 text-base lg:text-xl  items-center text-light font-semibold hover:*:-translate-y-1 [&>*]:transition-all [&>*]:ease-in-out gap-'>
-            <NavLinkItem children="Meu Perfil" link="" />
+            <NavLinkItem children="Meu Perfil" link="/meu-perfil" />
             <NavLinkItem children="Editar Perfil" link="/meu-perfil/editar" />
             <NavLinkItem children="Meus Chats" link="/meu-perfil/chats" />
             <LinkItem to="/login" children="Logout" icon={<FiLogOut color="#FFFFFF" size={22} />}   />
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,14 +5,23 @@ import MyAds from '@/pages/MyAds';
 import LoginPage from "@/pages/LoginPage"
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AdPage from "@/pages/AdPage";
+import ProfileTemplate from "@/template/ProfileTemplate";
+import ProfileDisplay from "@/components/Molecules/ProfileDisplay";
+import ProfileEdition from "@/components/Molecules/ProfileEdition";
+import UserChats from "@/components/Molecules/UserChats";
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/" element={<LayoutTemplate />}>
+          <Route path="meu-perfil" element={<ProfileTemplate />}>
+            <Route index element={<ProfileDisplay />} />
+            <Route path="editar" element={<ProfileEdition />} />
+            <Route path="chats" element={<UserChats />} />
+          </Route>
           <Route index element={<Home />} />
-          <Route path="/meus-anuncios" element={<MyAds />} />
+          <Route path="meus-anuncios" element={<MyAds />} />
           <Route path="criar-anuncios" element={<CreateAnnouncementPage/>}/>
           <Route path="anuncio/:adId" element={<AdPage/>}/>
         </Route>
