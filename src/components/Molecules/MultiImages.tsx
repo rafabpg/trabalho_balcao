e2b2a57c1709323c6cf490ed2c773ad09daabd88
rafabpg <rafa23giordano@gmail.com>
2024-11-22 17:19:14 -0300

    feat: adding responsiveness layout

--- src/components/Molecules/MultiImages.tsx
+++ src/components/Molecules/MultiImages.tsx
@@ -5,12 +5,14 @@ const MultiImages = ({ setValue }: any) => {
   const [images, setImages] = useState<(string | null)[]>(
     new Array(5).fill(null)
   );
+
   useEffect(() => {
     setValue(
       "images",
       images.filter((image) => image !== null)
     );
   }, [images, setValue]);
+
   const handleImageUpload = (index: number, base64Image: string) => {
     const updatedImages = [...images];
     updatedImages[index] = base64Image;
@@ -24,22 +26,19 @@ const MultiImages = ({ setValue }: any) => {
   };
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 ">
-      <div className="grid grid-cols-2 ">
-        {images.slice(0, 4).map((image, index) => (
-          <div key={index} className=" w-48 h-48 relative">
-            <ImageUploader
-              image={image}
-              onImageUpload={(base64Image) =>
-                handleImageUpload(index, base64Image)
-              }
-              onRemove={() => handleRemoveImage(index)}
-            />
-          </div>
-        ))}
-      </div>
-
-      <div className="col-span-1 sm:col-span-2 lg:col-span-1">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      {images.slice(0, 4).map((image, index) => (
+        <div key={index} className="w-full h-48 relative">
+          <ImageUploader
+            image={image}
+            onImageUpload={(base64Image) =>
+              handleImageUpload(index, base64Image)
+            }
+            onRemove={() => handleRemoveImage(index)}
+          />
+        </div>
+      ))}
+      <div className="col-span-1 sm:col-span-2 lg:col-span-1 w-full h-48 relative">
         <ImageUploader
           image={images[4]}
           onImageUpload={(base64Image) => handleImageUpload(4, base64Image)}
