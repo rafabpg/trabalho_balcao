8cdeaa2abd20aa5127cddcbc4e2ee982faf6985e
Lucas Levy <lucaslevy@id.uff.br>
2025-01-12 12:53:16 -0300

    config jest lib for test

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
+
+export default config;
